7abeca59506192e97f1763b765f52b76723ae1ed
Shayane <shayaneteixeira56@gmail.com>
2022-03-07 13:42:04 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,32 @@
 import Pagination from "../../components/Pagination";
+import MovieCard from "../../components/MovieCard";
 
 function Listing(){
     return(
+        <>
        <Pagination />
+        <div className="container">
+           <div className="row">
+               <div className="col">
+                   <MovieCard/>
+                </div>   
+            </div>
+        </div>
+        <div className="container">
+           <div className="row">
+               <div className="col">
+                   <MovieCard/>
+                </div>   
+            </div>
+        </div>
+        <div className="container">
+           <div className="row">
+               <div className="col">
+                   <MovieCard/>
+                </div>   
+            </div>
+        </div>
+       </>
     );
 }
 
